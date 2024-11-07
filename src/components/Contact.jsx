0145a1bbd53da0dfc6e4ebf8f66f4d8bfe52cfdc
baksarii <ohci0022@naver.com>
2024-11-07 16:00:53 +0900

    연락처 완료

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,57 @@
 import React from "react"; // 리액트 라이브러리 불러오기 
 
-const Contact = () => { // Contact라는 상수 선언, 함수형 컴포넌트 
-    return <div>Contact</div>;
+const contactText = [
+    {
+        link: "https://open.kakao.com/o/gM7YLzwf",
+        title: "KAKAO : webstupids",
+    },
+    {
+        link: "mailto:webstoryboy@naver.com",
+        title: "mail : webstoryboy@naver.com",
+    },
+];
+
+const Contact = () => {
+    return (
+        <section id="contact">
+            <div className="contact__inner">
+                <h2 className="contact__title">Contact</h2>
+                <div className="contact__lines" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+                <div className="contact__text">
+                    <div className="text">
+                        {contactText.map((contact, key) => (
+                            <div key={key}>
+                                <a
+                                    href={contact.link}
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                >
+                                    {contact.title}
+                                </a>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="contact__lines bottom" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Contact; // 내보내기 -> 외부에서 재사용 가능, 다른 파일에서 해당 컴포넌트 불러와 사용 가능
